2773bd891d08b3054ddd8b14f011df5744ba7731
caprxquemini <stefaniabarabula12@gmail.com>
2023-05-21 17:54:24 +0300

    Add project files.

---
+++ AndOperatorWithRanges/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Enter current temperature:");
+int currentTemp = int32.Parse(Console.ReadLine());
+
+if (currentTemp <= 0)
+{
+    Console.WriteLine("It's freezing.");
+}
+
+else if (currentTemp > 0 && currentTemp < 5)
+{
+    Console.WriteLine("It's cold.");
+}
+
+else if (currentTemp >= 5 && currentTemp < 10)
+{
+    Console.WriteLine("It's chilly.");
+}
+
+else if (currentTemp >= 10 && currentTemp < 15)
+{
+    Console.WriteLine("It's cool.");
+}
+
+else if (currentTemp >= 15 && currentTemp < 28)
+{
+    Console.WriteLine("It's warm.");
+}
+
+else
+{
+    Console.WriteLine("It's hot.");
+}
---
+++ Week5IfOr/Program.cs
@@ -0,0 +1,26 @@
+﻿string username, password;
+
+Console.WriteLine("Enter username:");
+username = Console.ReadLine("");
+
+Console.WriteLine("Enter password:");
+password = Console.ReadLine("");
+
+//if (username == "helloitsme" && password == "cats1234")
+//{
+//    Console.WriteLine("Welcome!");
+//}
+//else
+//{
+//    Console.WriteLine("Invalid username or password. Try again.");
+//}
+
+if (username == "helloitsme" || password == "cats1234")
+{
+    Console.WriteLine("Invalid username or password. Try again.");
+}
+
+else
+{
+    Console.WriteLine("Welcome!");
+}
